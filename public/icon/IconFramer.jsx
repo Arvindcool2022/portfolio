fcac1091c057d7ec51470d3ea1d024bc98d982d0
Arvind <arvindcool2022@gmail.com>
2023-10-31 18:45:30 +0530

    added media

---
+++ public/icon/IconFramer.jsx
@@ -0,0 +1,15 @@
+function IconFramer(props) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M4 0h16v8h-8zm0 8h8l8 8H4zm0 8h8v8z" />
+    </svg>
+  );
+}
+
+export default IconFramer;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,75 +1,88 @@
 import bite from '../../public/bite.jpeg';
+import youtube from '../../public/youtube.jpeg';
+import haircare from '../../public/haircare.jpeg';
+import misc from '../../public/misc.jpeg';
+import portfolio from '../../public/portfolio.jpeg';
+import moshify from '../../public/mosify.jpeg';
+import athletaro from '../../public/athletaro.jpeg';
 
 const projects = [
   {
     id: 1,
-    name: 'Vidsy - an youtube like app',
+    name: 'YouTube - from live youtube API',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi voluptatem accusantium. Quo soluta at, facere vitae aliquam molestiae autem delectus fugit incidunt aspernatur accusamus iste. Necessitatibus deleniti nulla numquam.',
-    img: 'https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: youtube,
     technology: '',
     code: 'github',
     demo: 'link'
+    // react,redux.react-router,react-player,tailwind css, rapid API,create-react-app
   },
   {
     id: 2,
-    name: 'Bite-Dash - an food ordering app',
+    name: 'Bite-Dash - from live swiggy API',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi voluptatem accusantium. Quo soluta at, facere vitae aliquam molestiae autem delectus fugit incidunt aspernatur accusamus iste. Necessitatibus deleniti nulla numquam.',
     img: bite,
     technology: '',
     code: 'github',
     demo: 'link'
+    // react,redux.react-router,tailwind css, Swiggy API,parcel
   },
   {
     id: 3,
     name: 'portfolio - current site',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi voluptatem accusantium. Quo soluta at, facere vitae aliquam molestiae autem delectus fugit incidunt aspernatur accusamus iste. Necessitatibus deleniti nulla numquam.',
-    img: bite,
+    img: portfolio,
     technology: '',
     code: 'github',
     demo: false
+    // react,tailwind css, framer-motion,vite
   },
   {
     id: 4,
     name: 'Haircare - an landing page for a saloon',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi voluptatem accusantium. Quo soluta at, facere vitae aliquam molestiae autem delectus fugit incidunt aspernatur accusamus iste. Necessitatibus deleniti nulla numquam.',
-    img: bite,
+    img: haircare,
     technology: '',
     code: 'github',
     demo: 'link'
+    //HTML, Sass, bootStrap
   },
   {
     id: 5,
     name: 'athletaro - an e-commerce like site',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi voluptatem accusantium. Quo soluta at, facere vitae aliquam molestiae autem delectus fugit incidunt aspernatur accusamus iste. Necessitatibus deleniti nulla numquam.',
-    img: bite,
+    img: athletaro,
     technology: '',
     code: 'github',
     demo: 'link'
+    //HTML,js, bootStrap
   },
   {
     id: 6,
-    name: 'misc - collection small projects in js',
+    name: 'misc - collection of fun projects in js',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi voluptatem accusantium. Quo soluta at, facere vitae aliquam molestiae autem delectus fugit incidunt aspernatur accusamus iste. Necessitatibus deleniti nulla numquam.',
-    img: bite,
+    img: misc,
     technology: '',
     code: 'github',
     demo: 'link'
+    //HTML,CSS,JS
   },
   {
     id: 7,
     name: 'moshify - a landing page for a cloud hosting company',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi voluptatem accusantium. Quo soluta at, facere vitae aliquam molestiae autem delectus fugit incidunt aspernatur accusamus iste. Necessitatibus deleniti nulla numquam.',
-    img: bite,
+    img: moshify,
     technology: '',
     code: 'github',
     demo: 'link'
+    //HTML,CSS,JS
   }
 ];
 
